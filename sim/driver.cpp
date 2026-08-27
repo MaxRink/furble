3e0574cda6615069f6f652bf56c853c52b470437
Maximilian Rink <maximilian.rink@telekom.de>
2026-08-27 05:58:28 +0200

    fix(scan): satisfy strict host CI

--- sim/driver.cpp
+++ sim/driver.cpp
@@ -210,10 +210,9 @@ void validateSeed(const std::string &name, const std::string &value) {
   }
 
   constexpr const char *booleanSeeds[] = {
-      "gps",          "gps_nmea",          "fauxny",
-      "autoconnect",  "reconnect",         "sleep_conn",
-      "boot_splash",  "connect_fail",      "no_touch",
-      "saved_camera", "scan_start_probe",  "scan_distinct",
+      "gps",       "gps_nmea",     "fauxny",           "autoconnect",
+      "reconnect", "sleep_conn",   "boot_splash",      "connect_fail",
+      "no_touch",  "saved_camera", "scan_start_probe", "scan_distinct",
   };
   if (std::find(std::begin(booleanSeeds), std::end(booleanSeeds), name) != std::end(booleanSeeds)) {
     if (!booleanSeedValue(value)) {
